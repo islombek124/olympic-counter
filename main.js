f5aa65c2cf8492fdd5b695723f22fb76c15b1140
islombek124 <brilliantpul@gmail.com>
2023-09-16 19:23:43 +0500

    Updated: Finally update

--- main.js
+++ main.js
@@ -20,16 +20,24 @@ function redWinClicked() {
         //
         cols.push(['red'])
         //
+        colEl.innerHTML = '<span class="red">R</span>'
+        scoreTable.innerHTML += colEl.outerHTML
+        // 
     } else if (cols[cols.length - 1].includes('red')) {
         //
         cols[cols.length - 1].push('red')
         //
+        colEl.innerHTML += '<span class="red">R</span>'
+        // 
+        document.querySelectorAll('.col')[document.querySelectorAll('.col').length - 1].innerHTML += '<span class="red">R</span>'
+        // 
     } else {
         //
         cols.push(['red'])
+        // 
+        colEl.innerHTML = '<span class="red">R</span>'
+        scoreTable.innerHTML += colEl.outerHTML
     }
-
-    console.log(cols);
 }
 
 function greenWinBtnClicked() {
@@ -37,16 +45,22 @@ function greenWinBtnClicked() {
         //
         cols.push(['green'])
         //
+        colEl.innerHTML = '<span class="green">G</span>'
+        scoreTable.innerHTML += colEl.outerHTML
+        //
     } else if (cols[cols.length - 1].includes('green')) {
         //
         cols[cols.length - 1].push('green')
         //
+        document.querySelectorAll('.col')[document.querySelectorAll('.col').length - 1].innerHTML += '<span class="green">G</span>'
+        //
     } else {
         //
         cols.push(['green'])
-        // 
+        //
+        colEl.innerHTML = '<span class="green">G</span>'
+        scoreTable.innerHTML += colEl.outerHTML
+        //
     }
-
-    console.log(cols);
 }
 
